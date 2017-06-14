62af19bef4aaa05a5872976deb29eefe51d0645b
Denniskamau <denniskamau3@gmail.com>
2017-06-14 22:38:57 +0300

    Included publication info

--- stacks/setup.py
+++ stacks/setup.py
@@ -1,3 +1,8 @@
+from dento import __version__
+
+import os
+import sys
+
 
 try:
     from setuptools import setup
@@ -7,19 +12,27 @@ except ImportError:
 dependencies = ['docopt', 'termcolor']
 
 
+def publish():
+    os.system("python setup.py sdist upload")
+
+if sys.argv[-1] == "publish":
+    publish()
+    sys.exit()
+
 setup(
-    name='stacks',
-    version='0.0.1',
-    description='stacks for stack graphs',
-    url='https://github.com/Denniskamau/stacks.git',
+    name='dento',
+    version=".".join(str(x) for x in __version__),
+    description='dento for stack graphs',
+    long_description=open('README.rst').read(),
+    url='https://github.com/Denniskamau/stack',
     license="MIT License",
     author='Dennis Kamau',
     author_email='denniskamau3@gmail.com',
     install_requires=dependencies,
-    packages=['stacks', ],
+    packages=['dento', ],
     entry_points={
         'console_scripts': [
-            'stacks=stacks.main:start'
+            'dento=dento.main:start'
         ],
     },
     classifiers=(
